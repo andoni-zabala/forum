1f786ddc8828d304341bc5ee6af5ad77be119bea
Andoni Zabala <andonifzabala@gmail.com>
2025-09-16 13:54:04 -0300

    makes title and description not nillable in comm table

---
+++ db/migrate/20250916213837_make_title_and_description_non_nullable.rb
@@ -0,0 +1,6 @@
+class MakeTitleAndDescriptionNonNullable < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null :communities, :title, false
+    change_column_null :communities, :description, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_15_213837) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_16_213837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "communities", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
+    t.string "title", null: false
+    t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
