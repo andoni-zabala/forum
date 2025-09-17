a06971ba988a426a5b09f9239595b5ba84c567b3
Andoni Zabala <andonifzabala@gmail.com>
2025-09-17 09:43:25 -0300

    rubocop fixes

--- app/repositories/abstract_base_repository.rb
+++ app/repositories/abstract_base_repository.rb
@@ -7,22 +7,46 @@ class AbstractBaseRepository
   extend T::Sig
   extend T::Helpers
   extend T::Generic
+
   abstract!
 
-  Entity = type_member(:out)
+  Entity = type_member { { upper: T::Struct } }
 
-  sig { abstract.params(dto: T.untyped).returns(T::Array[Entity]) }
-  def read(dto:); end
+  sig { returns(T.untyped) }
+  def self.entity
+    T.unsafe(const_get(:Entity)).inner_type[:fixed]
+  end
 
-  sig { abstract.params(dto: IdDto).returns(T.nilable(Entity)) }
-  def find(dto:); end
+  sig { overridable.params(dto: T.untyped).returns(T::Array[Entity]) }
+  def read(dto:)
+    raise_error(__method__)
+  end
 
-  sig { abstract.params(dto: T.untyped).returns(Entity) }
-  def create(dto:); end
+  sig { overridable.params(dto: IdDto).returns(T.nilable(Entity)) }
+  def find(dto:)
+    raise_error(__method__)
+  end
 
-  sig { abstract.params(dto: T.untyped).returns(T.nilable(Entity)) }
-  def update(dto:); end
+  sig { overridable.params(dto: T.untyped).returns(Entity) }
+  def create(dto:)
+    raise_error(__method__)
+  end
 
-  sig { abstract.params(dto: IdDto).returns(T.nilable(Entity)) }
-  def destroy(dto:); end
+  sig { overridable.params(dto: T.untyped).returns(T.nilable(Entity)) }
+  def update(dto:)
+    raise_error(__method__)
+  end
+
+  sig { overridable.params(dto: IdDto).returns(T.nilable(Entity)) }
+  def destroy(dto:)
+    raise_error(__method__)
+  end
+
+  private
+
+  sig { params(method: T.nilable(Symbol)).returns(T.noreturn) }
+  def raise_error(method)
+    raise NotImplementedError,
+          "#{method} must be implemented in #{self.class} repository"
+  end
 end
